7ec96473fff767724c5f6c9acb475acb741731e6
littletwolee <littletwolee@gmail.com>
2017-05-08 16:07:18 +0800

    modify ExecCommand func

--- cmd.go
+++ cmd.go
@@ -26,9 +26,8 @@ func GetCmd() *Cmd {
 //                  command                *exec.Cmd        cmd point
 //                  paras                  []string         parparameters
 //                  cmddir                 string           dir of exec
-//                  isStdout               bool             need os out put
 // @Returns output:string err:error
-func (c *Cmd) ExecCommand(command string, pars []string, cmddir string, isStdout bool) (string, error) {
+func (c *Cmd) ExecCommand(command string, pars []string, cmddir string) (string, error) {
 	cmd := exec.Command(command, pars...)
 	if cmddir != "" {
 		cmd.Dir = cmddir
@@ -36,10 +35,6 @@ func (c *Cmd) ExecCommand(command string, pars []string, cmddir string, isStdout
 	var buf bytes.Buffer
 	cmd.Stdout = &buf
 	cmd.Stderr = &buf
-	if isStdout {
-		cmd.Stdout = os.Stdout // 重定向标准输出
-		cmd.Stderr = os.Stderr // 重定向标准输出
-	}
 	err := cmd.Run()
 	out := buf.Bytes()
 	if err != nil {
