183299398e1c870ca210b52b86aab3b4087f533b
littletwolee <littletwolee@gmail.com>
2017-06-13 17:50:04 +0800

    change package to viper

--- config.go
+++ config.go
@@ -1,100 +1,17 @@
 package commons
 
 import (
-	"bufio"
-	"io"
-	"os"
-	"strings"
+	"github.com/spf13/viper"
 )
 
-var (
-	ConsConfig *config
-	confList   []map[string]map[string]string
-	filePath   string
-)
-
-type config struct{}
+var Config *viper.Viper
 
 func init() {
-	ConsConfig = new(config)
-	filePath = "conf/app.conf"
-	readList()
-}
-
-// @Title GetValue
-// @Description get value from config by section & name
-// @Parameters
-//            section            string          group name
-//            name               string          node name
-// @Returns err:error
-func (c *config) GetValue(section, name string) string {
-	// if conflist == nil{
-	// 	c.ReadList()
-	// }
-	for _, v := range confList {
-		for key, value := range v {
-			if key == section {
-				return value[name]
-			}
-		}
+	Config = viper.New()
+	Config.WatchConfig()
+	Config.SetConfigName("app")
+	Config.AddConfigPath("config/")
+	if err := Config.ReadInConfig(); err != nil {
+		panic(err)
 	}
-	return "no value"
-}
-
-// @Title readList
-// @Description create a kv list from config
-// @Returns config list:[]map[string]map[string]string
-func readList() {
-	file, err := os.Open(filePath)
-	consLogger := GetLogger()
-	if err != nil {
-		consLogger.LogErr(err)
-	}
-	defer file.Close()
-	var data map[string]map[string]string
-	var section string
-	buf := bufio.NewReader(file)
-	for {
-		l, err := buf.ReadString('\n')
-		line := strings.TrimSpace(l)
-		if err != nil {
-			if err != io.EOF {
-				consLogger.LogErr(err)
-			}
-			if len(line) == 0 {
-				break
-			}
-		}
-		switch {
-		case len(line) == 0:
-		case line[0] == '[' && line[len(line)-1] == ']':
-			section = strings.TrimSpace(line[1 : len(line)-1])
-			data = make(map[string]map[string]string)
-			data[section] = make(map[string]string)
-		default:
-			i := strings.IndexAny(line, "=")
-			value := strings.TrimSpace(line[i+1 : len(line)])
-			data[section][strings.TrimSpace(line[0:i])] = value
-			if uniquappend(section) == true {
-				confList = append(confList, data)
-			}
-		}
-
-	}
-}
-
-// @Title uniquappend
-// @Description check section is unique
-// @Parameters
-//            conf            string          section name
-// @Returns result:bool
-func uniquappend(conf string) bool {
-	for _, v := range confList {
-		for k, _ := range v {
-			if k == conf {
-				return false
-			}
-		}
-	}
-	return true
 }
