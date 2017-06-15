e41cb41bd8d882097e890a0004556e8ff9c336bc
littletwolee <littletwolee@gmail.com>
2017-06-15 15:51:52 +0800

    add Insert & Delete func

--- mysql.go
+++ mysql.go
@@ -8,21 +8,29 @@ import (
 )
 
 var (
-	consMysqlHelper *MysqlHelper
+	consMysqlHelper *mysqlHelper
 )
 
-type MysqlHelper struct {
+type mysqlHelper struct {
 	MasterDB, SlaveDb *gorm.DB
 }
 
 type SQLRule struct {
-	Table string
-	Where []string
+	Table  string
+	Where  *Where
+	Limit  interface{}
+	OffSet interface{}
 }
 
-func GetMysqlHelper() *MysqlHelper {
+type Where struct {
+	Sentence   string
+	Parameters []interface{}
+}
+
+func GetMysqlHelper() *mysqlHelper {
 	if consMysqlHelper == nil {
-		consMysqlHelper = &MysqlHelper{}
+		consMysqlHelper = &mysqlHelper{}
+		consMysqlHelper.MysqlInit()
 	}
 	return consMysqlHelper
 }
@@ -31,26 +39,22 @@ func GetMysqlHelper() *MysqlHelper {
 // 	Master, Slave, DataBase, UserName, PassWord string
 // }
 
-func init() {
-
-}
-
 // @Title MysqlInit
 // @Description get MasterDB & SlaveDB
 // @Parameters
 // @Returns
-func (m *MysqlHelper) MysqlInit() {
+func (m *mysqlHelper) MysqlInit() {
 	master := Config.GetString("mysql.master")
 	slave := Config.GetString("mysql.slave")
 	dataBase := Config.GetString("mysql.database")
 	userName := Config.GetString("mysql.username")
 	passWord := Config.GetString("mysql.password")
-	masterDB, err := getDB(userName, passWord, master, dataBase)
+	masterDB, err := getDB(master, userName, passWord, dataBase)
 	if err != nil {
 		GetLogger().LogPanic(err)
 	}
 	m.MasterDB = masterDB
-	slaveDB, err := getDB(userName, passWord, slave, dataBase)
+	slaveDB, err := getDB(slave, userName, passWord, dataBase)
 	if err != nil {
 		GetLogger().LogPanic(err)
 	}
@@ -65,7 +69,7 @@ func (m *MysqlHelper) MysqlInit() {
 //       password        string          db password
 // @Returns db:*gorm.DB err:error
 func getDB(address, userName, passWord, dataBase string) (*gorm.DB, error) {
-	db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@%s/%s?charset=utf8&parseTime=True&loc=Local", userName, passWord, address, dataBase))
+	db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", userName, passWord, address, dataBase))
 	db.DB().SetMaxOpenConns(2000)
 	db.DB().SetMaxIdleConns(1000)
 	if err != nil {
@@ -84,52 +88,89 @@ func getDB(address, userName, passWord, dataBase string) (*gorm.DB, error) {
 // 	return orm.Exec(query, args...)
 // }
 
-func (m *MysqlHelper) FindOne(sqlRule *SQLRule, result interface{}) error {
-	m.SlaveDb.Table(sqlRule.Table).Where(sqlRule.Where).First(&result)
+// @Title FindOne
+// @Description get model from mysql by parameters
+// @Parameters
+//       sqlRule         *SQLRule        sqlrule
+//       result          interface{}     return model
+// @Returns err:error
+func (m *mysqlHelper) FindOne(sqlRule *SQLRule, result interface{}) error {
+	err := m.SlaveDb.Table(sqlRule.Table).Where(sqlRule.Where.Sentence, sqlRule.Where.Parameters...).First(result).Error
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-// 	orm := beedb.New(db)
-// 	if q.Fields == "" {
-// 		q.Fields = "*"
-// 	}
-// 	return orm.SetTable(q.Table).Where(q.Where).OrderBy(q.OrderBy).Limit(q.Size, q.Offset).Select(q.Fields).Find(result)
-// }
+// @Title FindAll
+// @Description get model list from mysql by parameters
+// @Parameters
+//       sqlRule         *SQLRule        sqlrule
+//       result          []interface{}     return []model
+// @Returns err:error
+func (m *mysqlHelper) FindAll(sqlRule *SQLRule, result interface{}) error {
+	err := m.SlaveDb.Table(sqlRule.Table).Where(sqlRule.Where.Sentence, sqlRule.Where.Parameters...).Find(result).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
-// func (q *MysqlQuery) FindAll(result interface{}, newOrm bool) error {
-// 	db := SlaveDb
-// 	if newOrm {
-// 		db = ConnectMysql(false)
-// 		defer db.Close()
-// 	}
+// @Title FindByPaging
+// @Description get model list from mysql by parameters(limit & offset)
+// @Parameters
+//       sqlRule         *SQLRule        sqlrule
+//       result          []interface{}     return []model
+// @Returns err:error
+func (m *mysqlHelper) FindByPaging(sqlRule *SQLRule, result interface{}) error {
+	err := m.SlaveDb.Table(sqlRule.Table).Where(sqlRule.Where).Limit(sqlRule.Limit).Offset(sqlRule.OffSet).Find(result).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
-// 	orm := beedb.New(db)
-// 	if q.Fields == "" {
-// 		q.Fields = "*"
-// 	}
-// 	return orm.SetTable(q.Table).Where(q.Where).OrderBy(q.OrderBy).Limit(q.Size, q.Offset).Select(q.Fields).FindAll(result)
-// }
+// @Title Insert
+// @Description add model into mysql by model
+// @Parameters
+//       sqlRule         *SQLRule        sqlrule
+//       model          interface{}      insert model
+// @Returns err:error
+func (m *mysqlHelper) Insert(sqlRule *SQLRule, model interface{}) error {
+	err := m.SlaveDb.Table(sqlRule.Table).Save(model).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
-// func (q *MysqlQuery) Upsert(data interface{}, newOrm bool) error {
-// 	db := MasterDB
-// 	if newOrm {
-// 		db = ConnectMysql(true)
-// 		defer db.Close()
-// 	}
+// @Title Update
+// @Description update model by parameters
+// @Parameters
+//       sqlRule         *SQLRule        sqlrule
+//       result          []interface{}     return []model
+// @Returns
+func (m *mysqlHelper) Update(sqlRule *SQLRule, v interface{}, u map[string]interface{}) error {
+	err := m.MasterDB.Table(sqlRule.Table).Model(v).Where(sqlRule.Where.Sentence, sqlRule.Where.Parameters...).Update(u).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
-// 	orm := beedb.New(db)
-// 	return orm.SetTable(q.Table).Save(data)
-// }
-
-// func (q *MysqlQuery) Delete(newOrm bool) (int64, error) {
-// 	db := MasterDB
-// 	if newOrm {
-// 		db = ConnectMysql(true)
-// 		defer db.Close()
-// 	}
-// 	orm := beedb.New(db)
-// 	return orm.SetTable(q.Table).Where(q.Where).DeleteRow()
-// }
+// @Title Delete
+// @Description delete model by model
+// @Parameters
+//       sqlRule         *SQLRule        sqlrule
+//       model          []interface{}     return []model
+// @Returns
+func (m *mysqlHelper) Delete(sqlRule *SQLRule, model interface{}) error {
+	err := m.MasterDB.Table(sqlRule.Table).Delete(model).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 // // @Title name
 // // @Description
