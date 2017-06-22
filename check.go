914adf1dc1b977c0e2a2ddbe1087be3c82577f76
littletwolee <littletwolee@gmail.com>
2017-06-22 17:09:31 +0800

    add sql attack check module

---
+++ check.go
@@ -0,0 +1,75 @@
+package commons
+
+import (
+	"fmt"
+	"reflect"
+	"regexp"
+)
+
+var (
+	consCheck *check
+)
+
+type check struct {
+	CheckList string
+}
+
+func GetCheck() *check {
+	if consCheck == nil {
+		consCheck = &check{
+			CheckList: Config.GetString("check.sqlinjection"),
+		}
+	}
+	return consCheck
+}
+
+// // @Title SQLInjectionAttack
+// // @Description check interface sql injection attack
+// // @Parameters
+// //          value          interface{}           parameter
+// // @Returns value:interface{} err:error
+func (m *check) SQLInjectionAttack(value interface{}) (interface{}, error) {
+	if value == nil || value == "" {
+		return nil, fmt.Errorf(ERROR_PARAMETER_EMPTY)
+	}
+	switch value.(type) {
+	case string:
+		return m.attackCheck(value.(string))
+	default:
+		object := reflect.ValueOf(value)
+		for i := 0; i < object.NumField(); i++ {
+			field := object.Field(i)
+			_, err := m.attackCheck(field.Interface())
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return value, nil
+}
+
+func (m *check) attackCheck(value interface{}) (interface{}, error) {
+	var (
+		re  *regexp.Regexp
+		err error
+		v   string
+	)
+	str := `(?:')|(?:--)|(/\\*(?:.|[\\n\\r])*?\\*/)|(\b(` + m.CheckList + `)\b)`
+	re, err = regexp.Compile(str)
+	if err != nil {
+		value = ""
+		goto RESULT
+	}
+	v = fmt.Sprint(value)
+	if v == "" {
+		return "", fmt.Errorf(ERROR_PARAMETER_EMPTY)
+	}
+	if re.MatchString(v) {
+		err = fmt.Errorf(ERROR_PARAMETER_SQL_ATTACK, value)
+		value = ""
+		goto RESULT
+	}
+	goto RESULT
+RESULT:
+	return value, err
+}
