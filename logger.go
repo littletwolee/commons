8111ea3cfa917e60c8b04caf09a5cf166570e7fd
littletwolee <904693492@qq.com>
2018-07-22 15:48:52 +0800

    modify

--- logger.go
+++ logger.go
@@ -16,60 +16,71 @@ var (
 )
 
 const (
-	timeFormat string = "2006-01-02T15:04:05Z"
-	fieldTime  string = "time"
-	fieldFile  string = "file"
-	fieldFunc  string = "func"
-	fieldLine  string = "line"
+	timeFormat  string = "2006-01-02T15:04:05Z"
+	fieldTime   string = "time"
+	fieldFile   string = "file"
+	fieldFunc   string = "func"
+	fieldLine   string = "line"
+	errLogger   string = "error"
+	infoLogger  string = "info"
+	panicLogger string = "panic"
 )
 
 type logger struct {
-	e, i, p *logrus.Logger
+	m map[string]*logrus.Logger
+	//e, i, p *logrus.Logger
 }
 
 type ilogger interface {
 	// &logger{}
-	Error(format interface{}, a ...interface{})
-	Info(format interface{}, a ...interface{})
-	Panic(format interface{}, a ...interface{})
+	Error(i interface{})
+	Info(i interface{})
+	Panic(i interface{})
+	ErrorF(format string, a ...interface{})
+	InfoF(format string, a ...interface{})
+	PanicF(format string, a ...interface{})
 	init()
 }
 
-func (l *logger) field(format interface{}, a ...interface{}) *logrus.Entry {
+func field(l *logrus.Logger) *logrus.Entry {
 	pc, fileName, line, _ := runtime.Caller(1)
 	funcName := runtime.FuncForPC(pc).Name()
-	return l.e.WithFields(logrus.Fields{
+	return l.WithFields(logrus.Fields{
 		fieldFile: fileName,
 		fieldFunc: funcName[:len(funcName)-2],
 		fieldLine: line})
 }
 
-func (l *logger) Error(format interface{}, a ...interface{}) {
-	pc, fileName, line, _ := runtime.Caller(1)
-	funcName := runtime.FuncForPC(pc).Name()
-	l.e.WithFields(logrus.Fields{
-		fieldFile: fileName,
-		fieldFunc: funcName[:len(funcName)-2],
-		fieldLine: line}).Errorf(fmt.Sprintln(format), a)
+func (l *logger) Error(i interface{}) {
+	field(l.m[errLogger]).Error(i)
 }
 
-func (l *logger) Info(format interface{}, a ...interface{}) {
-	l.i.Infof(fmt.Sprint(format), a)
+func (l *logger) Info(i interface{}) {
+	field(l.m[infoLogger]).Info(i)
 }
 
-func (l *logger) Panic(format interface{}, a ...interface{}) {
-	pc, fileName, line, _ := runtime.Caller(1)
-	funcName := runtime.FuncForPC(pc).Name()
-	l.p.WithFields(logrus.Fields{
-		fieldFile: fileName,
-		fieldFunc: funcName[:len(funcName)-2],
-		fieldLine: line}).Panicf(fmt.Sprint(format), a)
+func (l *logger) Panic(i interface{}) {
+	field(l.m[panicLogger]).Panic(i)
+}
+
+func (l *logger) ErrorF(format string, a ...interface{}) {
+	field(l.m[errLogger]).Errorf(format, a...)
+}
+
+func (l *logger) InfoF(format string, a ...interface{}) {
+	field(l.m[infoLogger]).Infof(format, a...)
+}
+
+func (l *logger) PanicF(format string, a ...interface{}) {
+	field(l.m[panicLogger]).Panicf(format, a...)
 }
 
 func (l *logger) getLogger() {
-	l.e = l.getNew("error")
-	l.i = l.getNew("info")
-	l.p = l.getNew("panic")
+	m := make(map[string]*logrus.Logger)
+	m[errLogger] = l.getNew(errLogger)
+	m[infoLogger] = l.getNew(infoLogger)
+	m[panicLogger] = l.getNew(panicLogger)
+	l.m = m
 }
 
 func (l *logger) getNew(logLevel string) *logrus.Logger {
@@ -119,7 +130,6 @@ func Log(logPath string) ilogger {
 
 type log struct {
 	logger
-	m       sync.Mutex
 	logPath string
 }
 
@@ -137,6 +147,10 @@ func _log(logPath string) *log {
 
 func (l *log) init() {
 	l.getLogger()
+	for k, v := range l.m {
+		file := l.getFile(k)
+		v.Out = file
+	}
 }
 
 func (l *log) getFile(fileName string) *os.File {
@@ -152,20 +166,26 @@ func (l *log) getFile(fileName string) *os.File {
 	return file
 }
 
-func (l *log) Error(format interface{}, a ...interface{}) {
-	file := l.getFile(l.logger.e.Level.String())
-	l.e.Out = file
-	l.logger.Error(format, a)
+func (l *log) Error(i interface{}) {
+	field(l.m[errLogger]).Error(i)
 }
 
-func (l *log) Info(format interface{}, a ...interface{}) {
-	file := l.getFile(l.logger.i.Level.String())
-	l.e.Out = file
-	l.logger.Info(format, a)
+func (l *log) Info(i interface{}) {
+	field(l.m[infoLogger]).Info(i)
 }
 
-func (l *log) Panic(format interface{}, a ...interface{}) {
-	file := l.getFile(l.logger.p.Level.String())
-	l.e.Out = file
-	l.logger.Panic(format, a)
+func (l *log) Panic(i interface{}) {
+	field(l.m[panicLogger]).Panic(i)
+}
+
+func (l *log) ErrorF(format string, a ...interface{}) {
+	field(l.m[errLogger]).Errorf(format, a...)
+}
+
+func (l *log) InfoF(format string, a ...interface{}) {
+	field(l.m[infoLogger]).Infof(format, a...)
+}
+
+func (l *log) PanicF(format string, a ...interface{}) {
+	field(l.m[panicLogger]).Panicf(format, a...)
 }
